0b372340adba9df57697e0b8a6e50545e7d5a869
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-27 08:41:34 +0300

    Chore:Created a custom provider using useValue

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+
+class MockCoffeesService{}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [{provide: CoffeesService, useValue:new MockCoffeesService() }],
   exports:[CoffeesService]
 })
 export class CoffeesModule {}
