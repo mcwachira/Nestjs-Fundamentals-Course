bfd148134ae97268a4156f8d58c8d7a55cd04fd8
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-27 10:30:27 +0300

    Chore:Added async provider to delay the start of the application until the database is connected

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,24 +5,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffeees.constants';
-
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    return ['budy bew', 'nescafe'];
-  }
-}
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (dataSource: DataSource): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+
+        console.log('[!] Async Factory ');
+        return coffeeBrands;
+      },
+      inject: [DataSource],
     },
   ],
   exports: [CoffeesService],
